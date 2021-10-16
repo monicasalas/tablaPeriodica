b53886c5331b1f6cb45d147f095c7d4586cb4354
Mónica Janeth Salas Rodríguez <janethsalas.luna26@gmail.com>
2021-10-16 00:23:08 -0500

    Se agregan prefijos CRUD Elements

--- tablaPeriodicaPractica/routes/routes.js
+++ tablaPeriodicaPractica/routes/routes.js
@@ -4,6 +4,14 @@ const link = express.Router();
 
 const elements = require("../controllers/elements")
 
-link.route("/elements").post(elements.createElements);
+link
+    .route("/elements")
+    .get(elements.getElements)
+    .post(elements.createElements);
+
+link
+    .route("/elements/:id")
+    .put(elements.updateElement)
+    .delete(elements.deleteElement)
 
 module.exports = link;
