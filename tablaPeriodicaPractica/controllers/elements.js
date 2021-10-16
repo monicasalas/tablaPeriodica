a6b56fbee7974d87c33406abd7b12fb9a5b4c9d1
Mónica Janeth Salas Rodríguez <janethsalas.luna26@gmail.com>
2021-10-16 00:21:45 -0500

    Se agrega CRUD para elementos

--- tablaPeriodicaPractica/controllers/elements.js
+++ tablaPeriodicaPractica/controllers/elements.js
@@ -32,3 +32,71 @@ exports.createElements = async(req, res) => {
         return res.status(500).send(error.message)      
    }
 };
+
+exports.getElements = async(req, res) => {
+    //Búsqueda básica
+    try{
+        const find = await element.findAll({
+            where:{statusDelete:false},
+        });
+        return res.status(200).send(find)
+    }catch(error){
+        return res.status(500).send(message.error)
+    }
+};
+
+exports.updateElement = async(req, res) =>{
+    try {
+        const { body, params } = req;
+        if(!body)
+            return res.status(400).send({message:'Los datos son requeridos'})
+        if(!body.nameE)
+            return res.status(400).send({message:'nameE del elemento es requerido'})
+        if(!body.symbol)
+            return res.status(400).send({message:'symbol es requerido'})
+        if(!body.atomicNumber)
+            return res.status(400).send({message:'atomicNumber es requerido'})
+        if(!body.atomicMass)
+            return res.status(400).send({message:'atomicMass es requerido'})
+
+        const validate = await element.findOne({
+            where:{id: params.id},
+        })
+
+        if(!validate) return res.status(404).send({message: 'No se encontró el miembro' })
+        if(validate.statusDelete===true)
+            return res.status(404).send({message:'No se encontré el miembro'})
+        
+        validate.nameE = body.nameE,
+        validate.symbol = body.symbol,
+        validate.atomicNumber = body.atomicNumber,
+        validate.atomicMass = body.atomicMass
+
+        validate.save()
+
+        return res.status(202).send({message:'Elementos se actualizó correctamente'})
+
+    } catch (error) {
+        return res.status(500).send(message.error);
+    }
+};
+
+exports.deleteElement = async(req, res) =>{
+    try {
+        const {id} = req.params
+
+        const changeS = await element.findByPk(id);
+
+        if(!changeS) return res.status(404).send({message:'No se encontró el elemento'})
+        if(changeS.statusDelete===true)
+            return res.status(404).send({message:'No se encontró el elemento'})
+        
+        changeS.statusDelete = true;
+        changeS.save();
+
+        return res.status(200).send({message:'No se encontró el elemento'})
+
+    } catch (error) {
+        return res.status(500).send(message.error)
+    }
+};
