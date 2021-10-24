1bd88f8c27d676a76ba4029fc333f24127793f65
Mónica Janeth Salas Rodríguez <janethsalas.luna26@gmail.com>
2021-10-24 01:10:25 -0500

    Se agrega módulo para subir imagen

---
+++ tablaPeriodicaPractica/utils/uploadImage.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.fileUpload = async function(file, namePath){
+    try {
+        let matches = file.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+
+        let response = {};
+
+        if(matches.length !==3) return new Error("String inválido")
+
+        response.type = matches[1];
+        response.data = Buffer.from(matches[2], 'base64');
+        let imageBuffer = response.data;
+
+        if(!fs.existsSync(`${path.dirname(require.main.filename)}/public${namePath}`)){
+            fs.mkdirSync(`${path.dirname(require.main.filename)}/public${namePath}`,
+            true
+            );
+        }
+
+        let extension = response.type.split('/');
+        let fileName = `${Date.now()}.${extension[1]}`;
+        let fileRoute = `${namePath}/${fileName}`;
+
+        fs.writeFileSync(
+            `${path.dirname(require.main.filename)}/public${fileRoute}`,
+            imageBuffer,
+            "utf8"
+        );
+        
+        return fileRoute;
+
+    } catch (error) {
+        return new Error('Error del servidor')
+    }
+}
