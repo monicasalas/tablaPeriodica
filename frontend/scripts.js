389ed4457c69b7a4d21eeef989cf8538407d7b9b
Mónica Janeth Salas Rodríguez <janethsalas.luna26@gmail.com>
2022-01-07 23:20:50 -0600

    Se pinta tabla periódica

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,16 +1,38 @@
+let elements = [];
+let symbols =[];
+let atomicNumber = [];
+let atomicMass = [];
+
+
 const getAll = async ()=> {
     const response = await fetch(
         `http://127.0.0.1:3000/tablaperiodica/elements`
     );
 
-    const myInfo = await response.json();
-    const container = document.getElementById('ele');
+    const myInfoE = await response.json();
+    elements = myInfoE;
 
-    myInfo.forEach((element) => {
-        const div = document.createElement('h3');
-        div.innerHTML = element.nameE;
-        container.appendChild(div);
 
+    myInfoE.forEach((element) => {
+        const container = document.getElementById(element.atomicNumber);
+        if(container!==null){
+            const elemento = document.createElement('h3');
+            const symb = document.createElement('h1');
+            const vnato = document.createElement('h4');
+            const mass = document.createElement('h2')
+
+            elemento.innerHTML = element.nameE;
+            symb.innerHTML = element.symbol;
+            vnato.innerHTML = element.atomicNumber;
+            mass.innerHTML = element.atomicMass;
+
+            container.appendChild(elemento);
+            container.appendChild(symb);
+            container.appendChild(vnato);
+            container.appendChild(mass);
         
+        }
+            
     });
+    
 }
